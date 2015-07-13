bf5ed9d99940ee398bb3f2e5b2aae7451d847fc5
Abdy F. Sanchez <abdyfernando@gmail.com>
2015-07-12 18:48:32 -0600

    First Release
    The Order Pages of HostBill, Now available for Blesta.

---
+++ clean/language/en_us/main.php
@@ -0,0 +1,17 @@
+<?php
+$lang['Main.index.heading'] = "Package Selection";
+$lang['Main.index.subheading'] = "Please select a package group below to continue.";
+
+$lang['Main.index.field_currency'] = "Currency:";
+$lang['Main.index.show_cart_btn'] = "Show Cart";
+
+$lang['Main.packages.subheading'] = "Select a package and configure it on the next step.";
+$lang['Main.packages.select_note'] = "Drag to select a different package.";
+$lang['Main.packages.price_start'] = "Starting at";
+
+$lang['Main.packages.box_select'] = "Select";
+$lang['Main.packages.box_selected'] = "Selected";
+
+$lang['Main.packages.continue_btn'] = "Continue";
+$lang['Main.packages.continue_note'] = "Continue to configuration and checkout.";
+?>

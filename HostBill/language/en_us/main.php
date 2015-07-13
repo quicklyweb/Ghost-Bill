4dd7887cfd386c6e41b647f49e066bdb9130d3e4
Abdy F. Sanchez <abdyfernando@gmail.com>
2015-07-12 23:36:44 -0600

    Hostbill

---
+++ HostBill/language/en_us/main.php
@@ -0,0 +1,19 @@
+<?php
+$lang['Main.index.heading'] = "Package Selection";
+$lang['Main.index.subheading'] = "Please select a package group below to continue.";
+$lang['Main.index.group_heading'] = "Select a package below.";
+$lang['Main.index.field_pricing_id'] = "Choose your billing cycle.";
+$lang['Main.index.button_order'] = "Order Now";
+
+$lang['Main.index.field_currency'] = "Currency:";
+
+$lang['Main.packages.subheading'] = "Select a package and then configure it down below.";
+$lang['Main.packages.select_note'] = "Drag to select a different package.";
+$lang['Main.packages.price_start'] = "Starting at";
+
+$lang['Main.packages.box_select'] = "Select";
+$lang['Main.packages.box_selected'] = "Selected";
+$lang["Main.index.order_placed"] = "Order Placed on";
+$lang["Main.index.order_status"] = "STATUS: ";
+$lang["Main.index.order_sumamry"] = "Order Summary";
+?>
